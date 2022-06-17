b41a52e0ae4522756e0ed351b512be689be431a4
ricardorichie <richardadesina26@gmail.com>
2022-06-17 16:29:13 +0100

    readme added

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -11,10 +11,10 @@ const source = fs.readFileSync(campaignPath, "utf-8");
 const output = solc.compile(source, 1).contracts;
 
 fs.ensureDirSync(buildPath);
-console.log(output);
+// console.log(output);
 for (let contract in output) {
   fs.outputJsonSync(
-    path.resolve(buildPath, contract + ".json"),
+    path.resolve(buildPath, contract.replace(":", "") + ".json"),
     output[contract]
   );
 }
